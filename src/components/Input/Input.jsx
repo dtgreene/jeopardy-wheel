d5d727761b3d6b5484c7c26662eb728c80bd0cf5
Dylan <dylantgreene92@gmail.com>
2022-11-11 12:54:02 -0600

    Color changes

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -5,7 +5,7 @@ import styles from './Input.module.css';
 export const Input = ({ className, ...other }) => (
   <input
     className={cx(
-      'rounded bg-gray-800 border border-gray-600 text-inherit outline-none focus:ring-blue-600 focus:border-blue-600 block w-full px-3 py-2 transition-colors',
+      'rounded bg-gray-800 border border-gray-500 text-inherit outline-none focus:ring-sky-500 focus:border-sky-500 block w-full px-3 py-2 transition-colors',
       styles.input,
       className
     )}
