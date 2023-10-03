0b9f3c6cff09223720c243ae503de265b3b5606b
dtgreene <dylan.greene@shipt.com>
2023-10-03 18:08:50 -0500

    Combine vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
+const tailwindConfig = {
+  content: ['./src/**/*.{jsx,css}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +22,11 @@ export default defineConfig({
   resolve: {
     alias: {
       src: path.resolve(__dirname, 'src'),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss(tailwindConfig), autoprefixer()],
     },
   },
 });
